b82aec6bf8d3256fe64c497e7b0213cd41299672
Georgiy <abramyants90@mail.ru>
2022-12-02 21:36:40 +0300

    0.1

---
+++ gal_bot/src/test/java/org/galfest/gal_bot/GalBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.galfest.gal_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
